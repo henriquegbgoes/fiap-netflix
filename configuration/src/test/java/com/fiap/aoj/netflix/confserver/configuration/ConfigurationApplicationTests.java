0dc2a34b405966fd94d5350b2416e81dcfb2b3b6
Henrique Góes <henriquegb.goes@gmail.com>
2020-09-14 02:08:23 -0300

    Projeto Configuration

---
+++ configuration/src/test/java/com/fiap/aoj/netflix/confserver/configuration/ConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.aoj.netflix.confserver.configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
