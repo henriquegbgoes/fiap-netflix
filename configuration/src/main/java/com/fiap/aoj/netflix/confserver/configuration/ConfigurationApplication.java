e3e9b8c7dda6f8752882548651f718bc9219b4af
Henrique Góes <henriquegb.goes@gmail.com>
2020-09-15 14:58:01 -0300

    Incluindo Configuration no Service Discover

--- configuration/src/main/java/com/fiap/aoj/netflix/confserver/configuration/ConfigurationApplication.java
+++ configuration/src/main/java/com/fiap/aoj/netflix/confserver/configuration/ConfigurationApplication.java
@@ -2,10 +2,12 @@ package com.fiap.aoj.netflix.confserver.configuration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient	
 public class ConfigurationApplication {
 
 	public static void main(String[] args) {
