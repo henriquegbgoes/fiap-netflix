da4f07d473c9869e3b2f990e76878434134690f0
Henrique Góes <henriquegb.goes@gmail.com>
2020-09-15 00:51:43 -0300

    Adicionado server configuration em Suporte

--- suporte/src/main/java/com/fiap/aoj/nexflix/suporte/service/SuporteService.java
+++ suporte/src/main/java/com/fiap/aoj/nexflix/suporte/service/SuporteService.java
@@ -1,8 +1,10 @@
 package com.fiap.aoj.nexflix.suporte.service;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.fiap.aoj.nexflix.suporte.dto.Suporte;
@@ -11,11 +13,19 @@ import com.fiap.aoj.nexflix.suporte.repository.DBRepository;
 @Service
 public class SuporteService {
 
+	@Value("${sensibilizaInsert}")
+	private boolean SENSIBILIZA_INSERT;
+	
 	@Autowired
 	private DBRepository dbRepository;
 	
 	public String abrirChamado(Suporte suporte) {
-		List<Object> result = dbRepository.callProcedure(suporte, "sp_incluir_suporte");
+		List<Object> result;
+		if(!SENSIBILIZA_INSERT) 
+			result = Arrays.asList(Boolean.TRUE);
+		else 
+			result = dbRepository.callProcedure(suporte, "sp_incluir_suporte");
+		
 		if(result.contains(Boolean.TRUE))
 			return "Chamado Aberto com sucesso!!!";
 		else 
