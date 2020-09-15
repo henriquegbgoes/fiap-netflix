c73543ade2782948737476bd5076f6921f6de09d
Henrique Góes <henriquegb.goes@gmail.com>
2020-09-15 11:33:20 -0300

    Mudança de Package

--- eurekaserver-netflix/src/main/java/com/fiap/aoj/netflix/eurekaserver/EurekaserverApplication.java
+++ eurekaserver-netflix/src/main/java/com/fiap/aoj/netflix/eurekaserver/EurekaserverApplication.java
@@ -1,4 +1,4 @@
-package com.example.eurekaserver;
+package com.fiap.aoj.netflix.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
