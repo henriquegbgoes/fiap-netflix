c6c6d74d8999e8dd2b42cf3ee98284c846b58eed
Henrique Góes <henriquegb.goes@gmail.com>
2020-09-15 14:59:02 -0300

    Incluindo Suporte no service discover

--- suporte/src/main/java/com/fiap/aoj/nexflix/suporte/SuporteApplication.java
+++ suporte/src/main/java/com/fiap/aoj/nexflix/suporte/SuporteApplication.java
@@ -2,8 +2,10 @@ package com.fiap.aoj.nexflix.suporte;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SuporteApplication {
 
 	public static void main(String[] args) {
