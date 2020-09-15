8bac6f6aafe56f7598af2fb33bc803ce09e5c16d
Henrique Góes <henriquegb.goes@gmail.com>
2020-09-15 14:58:42 -0300

    Incluindo  MS Titulo no Service Discover

--- titulo/src/main/java/com/fiap/aoj/nexflix/titulo/TituloApplication.java
+++ titulo/src/main/java/com/fiap/aoj/nexflix/titulo/TituloApplication.java
@@ -2,8 +2,10 @@ package com.fiap.aoj.nexflix.titulo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class TituloApplication {
 
 	public static void main(String[] args) {
--- titulo/src/main/java/com/fiap/aoj/nexflix/titulo/controller/TituloController.java
+++ titulo/src/main/java/com/fiap/aoj/nexflix/titulo/controller/TituloController.java
@@ -4,12 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fiap.aoj.nexflix.titulo.dto.MarcarTitulo;
@@ -21,9 +19,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
 @RestController
-@RequestMapping("/titulos") 
 @Api(value = "API REST Titulos Netflix")
-@CrossOrigin(origins = "*")
 @RefreshScope
 public class TituloController {
 	
